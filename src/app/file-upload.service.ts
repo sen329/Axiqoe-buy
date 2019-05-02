9c04b19b7b7d0211ea53c0643900ae46cfe754fb
sen329 <hans.hilman@gmail.com>
2019-05-02 09:32:25 +0700

    Added ability to update database from csv

--- src/app/file-upload.service.ts
+++ src/app/file-upload.service.ts
@@ -11,14 +11,36 @@ import { AuthService } from './auth.service';
 })
 export class FileUploadService {
 
-  private baseUrl = 'http://127.0.0.1:8000/api/upload';
+  private baseUrl = 'http://127.0.0.1:8000/api';
 
   constructor(private httpClient: HttpClient, private auth: AuthService) { }
 
   public upload(data) {
-    let uploadURL = this.baseUrl;
+    let uploadURL = `${this.baseUrl}/upload`;
 
     return this.httpClient.post<any>(uploadURL, data, {
+      reportProgress: true,
+      observe: 'events'
+    }).pipe(map((event) => {
+
+      switch (event.type) {
+
+        case HttpEventType.UploadProgress:
+          const progress = Math.round(100 * event.loaded / event.total);
+          return { status: 'progress', message: progress };
+
+        case HttpEventType.Response:
+          return event.body;
+        default:
+          return `Unhandled event: ${event.type}`;
+      }
+    })
+    );
+  }
+  public update(data) {
+    let updateURL = `${this.baseUrl}/fileupdate`;
+
+    return this.httpClient.post<any>(updateURL, data, {
       reportProgress: true,
       observe: 'events'
     }).pipe(map((event) => {
--- src/app/productspage/productspage.component.ts
+++ src/app/productspage/productspage.component.ts
@@ -3,6 +3,8 @@ import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import { ProductsService } from '../products.service';
 import { Product } from '../product';
+import { FileUploadService } from '../file-upload.service';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-productspage',
@@ -12,15 +14,24 @@ import { Product } from '../product';
 export class ProductspageComponent implements OnInit {
 
   products: Product[];
+  fileToUpload: File = null;
+  forms: FormGroup;
+  error: string;
+  uploadResponse = { status: '', message: '', filePath: '' };
 
   constructor(
     private route: ActivatedRoute,
     private productsService: ProductsService,
-    private location: Location
+    private location: Location,
+    private fileUploadService: FileUploadService,
+    private formBuilder: FormBuilder,
   ) { }
 
   ngOnInit() {
     this.getProducts();
+    this.forms = this.formBuilder.group({
+      file: ['']
+    });
   }
 
   getProducts(): void{
@@ -29,4 +40,21 @@ export class ProductspageComponent implements OnInit {
   }
 
   headElements = ['ID', 'Product Code', 'Product Name', 'Price'];
+
+  onFileChange(event) {
+    if (event.target.files.length > 0) {
+      const file = event.target.files[0];
+      this.forms.get('file').setValue(file);
+    }
+  }
+
+  onSubmit() {
+    const formData = new FormData();
+    formData.append('file', this.forms.get('file').value);
+
+    this.fileUploadService.update(formData).subscribe(
+      (res) => this.uploadResponse = res,
+      (err) => this.error = err
+    ),{responseType: 'file'}, alert("Data Uploaded"); console.error;
+  }
 }
