e5b5ed48529f8bf921e187a58e2eddd9949b0222
sen329 <hans.hilman@gmail.com>
2019-06-24 10:18:24 +0700

    routing changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,7 +14,7 @@ import { ReportComponent } from './report/report.component';
 
 
 const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '', redirectTo: 'listofapproval', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'listofapproval', component: ListofapprovalComponent, canActivate: [AuthGuard] },
